45dfea1379749e119988cf974a8733064a4c37ce
aa404 <agbadoaurel@gmail.com>
2024-06-24 14:58:24 +0200

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from .routers import ai
 import logging
 
-app = FastAPI(title="IA API Quick Integration")
+app = FastAPI(title="AI API Quick Integration")
 
 logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -21,3 +21,7 @@ app.include_router(ai.router)
 @app.get("/")
 def read_root():
     return {"IA API Quick Integration": "0.1.0"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
